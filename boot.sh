f0d02f9f8e78163c3ba720e8ae6a42fb52ca9e91
Novitsh <Novitsh@2331286.no-reply.drupal.org>
2015-06-01 23:42:20 +0200

    Bootloader

---
+++ boot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+## Should debug trigger?
+#set -x 
+
+# Set this env path
+CWD=$(pwd)
+
+# Load libs
+#. ./logic/lib.sh
+#. ./logic/spinner.sh
+#. ./logic/bar
+
+# Empty the screen
+clear
+
+# Run startup
+#startup
+#python ai.py --voice-control --facial-recognition
