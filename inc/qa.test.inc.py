e436efac9e45fb9c7053b224c2f3df1e08ec43ef
Novitsh <Novitsh@2331286.no-reply.drupal.org>
2015-06-01 23:38:57 +0200

    Added Testing QA

---
+++ inc/qa.test.inc.py
@@ -0,0 +1,14 @@
+answers = {
+	'mood' : 
+		['good','ok','great','super']
+}
+
+synonyms = {
+	['what', 'who'],
+	['is','was','are']
+}
+
+
+
+if 'good' in answers['mood']: print 'yes'
+else: print 'no'
